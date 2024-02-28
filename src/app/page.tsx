a97fdb2a748b451020f971a8ce7e2c4b03b44efc
Max Shen <m4xshen@gmail.com>
2024-02-29 07:36:27 +0800

    feat: fetch blog title and date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit({});
+
+export default async function Home() {
+  const { data: posts } = await octokit.request(
+    'GET /repos/{owner}/{repo}/issues',
+    {
+      owner: process.env.OWNER,
+      repo: process.env.REPO,
+    },
+  );
+
   return (
-    <div className="mt-20 flex h-full flex-col items-center justify-center gap-10">
-      blog list
+    <div className="mx-auto mt-20 flex h-full w-max flex-col justify-center gap-10">
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h1 className="text-2xl font-semibold">{post.title}</h1>
+          <div className="mb-5 text-sm text-[#ffffffa0]">
+            {new Date(post.created_at).toDateString()}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
