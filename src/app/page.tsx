fb1112b7e891dd050d1186ff62dbd0c4c95707f6
Max Shen <m4xshen@gmail.com>
2024-03-01 07:40:51 +0800

    feat: post detail page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,41 @@
+import { Card, CardFooter, CardHeader } from '@nextui-org/card';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
 import { Octokit } from 'octokit';
 
-const octokit = new Octokit({});
+async function getPosts() {
+  const auth = cookies().get('access_token')?.value;
+  const octokit = new Octokit(auth ? { auth } : {});
+
+  const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+    owner: process.env.OWNER,
+    repo: process.env.REPO,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+  return data;
+}
 
 export default async function Home() {
-  const { data: posts } = await octokit.request(
-    'GET /repos/{owner}/{repo}/issues',
-    {
-      owner: process.env.OWNER,
-      repo: process.env.REPO,
-    },
-  );
+  const posts = await getPosts();
 
   return (
-    <div className="mx-auto mt-20 flex h-full w-max flex-col justify-center gap-10">
+    <div className="mx-auto mt-20 flex h-full w-max flex-col justify-center gap-5">
       {posts.map((post) => (
-        <div key={post.id}>
-          <h1 className="text-2xl font-semibold">{post.title}</h1>
-          <div className="mb-5 text-sm text-[#ffffffa0]">
+        <Card
+          key={post.number}
+          as={Link}
+          href={`post/${post.number}`}
+          className="bg-transparent"
+        >
+          <CardHeader className="text-2xl font-semibold">
+            {post.title}
+          </CardHeader>
+          <CardFooter className="text-sm text-[#ffffffa0]">
             {new Date(post.created_at).toDateString()}
-          </div>
-        </div>
+          </CardFooter>
+        </Card>
       ))}
     </div>
   );
---
+++ src/app/post/[number]/page.tsx
@@ -0,0 +1,35 @@
+import { Octokit } from 'octokit';
+import { cookies } from 'next/headers';
+
+async function getPost(issue_number: number) {
+  const auth = cookies().get('access_token')?.value;
+  const octokit = new Octokit(auth ? { auth } : {});
+
+  const { data } = await octokit.request(
+    'GET /repos/{owner}/{repo}/issues/{issue_number}',
+    {
+      owner: process.env.OWNER,
+      repo: process.env.REPO,
+      issue_number,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    },
+  );
+  return data;
+}
+
+export default async function Post({ params }: { params: { number: string } }) {
+  const post = await getPost(parseInt(params.number, 10));
+  return (
+    <div className="mx-auto mt-20 grid w-max gap-6">
+      <div className="grid gap-2">
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <div className="text-sm text-gray-400">
+          {new Date(post.created_at).toDateString()}
+        </div>
+      </div>
+      <div className="prose prose-invert">{post.body}</div>
+    </div>
+  );
+}
