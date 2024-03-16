5ad800c268a429a10aff8a7e7736691182aa79b9
Max Shen <m4xshen@gmail.com>
2024-03-16 18:59:51 +0800

    refactor: refine folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import NavbarWrapper from '@/components/NavbarWrapper';
-import Footer from '@/components/Footer';
+import NavbarWrapper from '@/components/layout/NavbarWrapper';
+import Footer from '@/components/layout/Footer';
 import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
