eb6ad76dee1453a2309463c2f363a90e179242ec
Max Shen <m4xshen@gmail.com>
2024-03-16 11:40:56 +0800

    fix: cookies build issue

--- src/actions/octokit.ts
+++ src/actions/octokit.ts
@@ -1,15 +1,33 @@
 'use server';
 
+import { cookies } from 'next/headers';
 import octokit from '@/utils/octokit';
+import { Octokit } from 'octokit';
 
-// eslint-disable-next-line import/prefer-default-export
+export async function getUser() {
+  const accessToken = cookies().get('access_token')?.value;
+  const userOctokit = new Octokit({ auth: accessToken });
+  try {
+    const { data } = await userOctokit.rest.users.getAuthenticated();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
 export async function getPosts(page: number) {
-  const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
-    owner: process.env.NEXT_PUBLIC_OWNER,
-    repo: process.env.NEXT_PUBLIC_REPO,
-    per_page: 10,
-    page,
-  });
+  try {
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+      owner: process.env.NEXT_PUBLIC_OWNER,
+      repo: process.env.NEXT_PUBLIC_REPO,
+      per_page: 10,
+      page,
+    });
 
-  return data;
+    return data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
--- src/components/NavbarWrapper.tsx
+++ src/components/NavbarWrapper.tsx
@@ -1,6 +1,6 @@
 import { Navbar, NavbarBrand, NavbarContent } from '@nextui-org/navbar';
-import getUser from '@/utils/getUser';
 import Link from 'next/link';
+import { getUser } from '@/actions/octokit';
 import AvatarWrapper from './AvatarWrapper';
 
 export default async function NavbarWrapper() {
