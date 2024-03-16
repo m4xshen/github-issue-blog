81239870b005dea3596d0ba6fbaa688658b4b940
Max Shen <m4xshen@gmail.com>
2024-03-16 15:33:07 +0800

    refactor: hide posts page scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${inter.className} flex min-h-screen flex-col bg-background`}
+        className={`${inter.className} flex min-h-screen flex-col bg-background scrollbar-hide`}
       >
         <Providers>
           <NavbarWrapper />
