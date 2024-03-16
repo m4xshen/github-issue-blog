0ec9c31a3ce7d6e6de80c72b9c5e2e4430f9ad1e
Max Shen <m4xshen@gmail.com>
2024-03-16 14:33:18 +0800

    feat: form validation

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Textarea } from '@nextui-org/input';
+import { Textarea, Input } from '@nextui-org/input';
 import { Button } from '@nextui-org/button';
 import { createPost } from '@/actions/post';
 import { useFormStatus } from 'react-dom';
+import { useState } from 'react';
 
-function Publish() {
+function Publish({ isInvalid }: { isInvalid: boolean }) {
   const { pending } = useFormStatus();
   return (
     <Button
@@ -13,6 +14,7 @@ function Publish() {
       radius="sm"
       color="primary"
       className="ml-auto"
+      isDisabled={isInvalid}
       isLoading={pending}
     >
       Publish
@@ -21,25 +23,39 @@ function Publish() {
 }
 
 export default function NewPost() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const titleIsInvalid = title === '';
+  const bodyIsInvalid = body.length < 30;
+
   return (
     <form
       className="prose prose-invert mx-auto mt-20 flex flex-col gap-6"
       action={createPost}
     >
-      <h1>
-        <input
-          name="title"
-          placeholder="New post title here..."
-          className="bg-transparent outline-none"
-        />
-      </h1>
+      <Input
+        name="title"
+        value={title}
+        onValueChange={setTitle}
+        isInvalid={titleIsInvalid}
+        errorMessage={titleIsInvalid && 'Title is required.'}
+        placeholder="New post title here..."
+        className="bg-transparent outline-none"
+      />
       <Textarea
+        value={body}
+        onValueChange={setBody}
+        isInvalid={bodyIsInvalid}
+        errorMessage={
+          bodyIsInvalid && 'Content must be at least 30 characters.'
+        }
         name="body"
         radius="sm"
         size="lg"
         placeholder="Write your content here."
       />
-      <Publish />
+      <Publish isInvalid={titleIsInvalid || bodyIsInvalid} />
     </form>
   );
 }
