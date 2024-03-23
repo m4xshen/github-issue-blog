42ca9d1c357aec4fca89cb3b4b638bd2d44d46f8
Max Shen <m4xshen@gmail.com>
2024-03-23 09:18:53 +0800

    feat: modal for confirming post delete

--- src/components/PostActions.tsx
+++ src/components/PostActions.tsx
@@ -2,12 +2,21 @@
 
 import { deletePost } from '@/actions/post';
 import { Button } from '@nextui-org/button';
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from '@nextui-org/modal';
 import Link from 'next/link';
 import { useTransition } from 'react';
 import { toast } from 'sonner';
 
 export default function PostActions({ number }: { number: number }) {
   const [isLoading, startTransition] = useTransition();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <div className="flex items-center gap-5">
@@ -20,21 +29,52 @@ export default function PostActions({ number }: { number: number }) {
       >
         Edit
       </Button>
-      <Button
-        radius="sm"
-        color="primary"
-        isLoading={isLoading}
-        onPress={() => {
-          startTransition(async () => {
-            const error = await deletePost(number);
-            if (error) {
-              toast('Error deleting post.');
-            }
-          });
-        }}
-      >
+      <Button radius="sm" color="primary" onPress={onOpen}>
         Delete
       </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        className="bg-background"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>Delete post</ModalHeader>
+              <ModalBody>
+                Are you sure you want to delete this post? This action cannot be
+                undone.
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  radius="sm"
+                  color="primary"
+                  variant="light"
+                  onPress={onClose}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  radius="sm"
+                  color="danger"
+                  isLoading={isLoading}
+                  onPress={() => {
+                    startTransition(async () => {
+                      const error = await deletePost(number);
+                      if (error) {
+                        toast('Error deleting post.');
+                      }
+                      onClose();
+                    });
+                  }}
+                >
+                  Delete
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
